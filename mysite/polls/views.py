6d9c452733c2b30a8d4cb7dd9df907543439d3e6
felipelyra <felipeinsonia@gmail.com>
2019-06-10 13:36:09 -0300

    Adicionando CSS

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,25 +4,25 @@ from django.template import loader
 from django.http import Http404
 from django.urls import reverse
 from .models import Question,Choice
+from django.views import generic
 
-# Create your views here.
-def index(request):
-    # displays the latest 5 poll questions in the system, separated by commas, according to publication date:
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-    #template = loader.get_template('polls/index.html')
-    #return HttpResponse(template.render(context, request))
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
-    return render(request, 'polls/index.html', context)
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
