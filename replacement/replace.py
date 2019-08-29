0b243d3444f1fe211ce2805a35fac194d02d76f5
felipelyrawizard <felipeinsonia@gmail.com>
2019-08-29 00:06:35 -0300

    Versão inicial

---
+++ replacement/replace.py
@@ -0,0 +1,44 @@
+import csv
+import fileinput
+import os
+
+def retorna_depara(nome):
+    de = []
+    para = []
+    with open(nome, 'r') as csvfile:
+        reader = csv.reader(csvfile, delimiter=';', quotechar='|') #tabs = \t
+        #pula o cabecalho
+        next(reader)
+        for row in reader:
+            de.append(row[0])
+            para.append(row[1])
+    return de,para
+
+def substituir(Dir, velho, novo):
+    for root, directories, filenames in os.walk(Dir):
+        for filename in filenames:
+            print(os.path.join(root,filename))
+            with fileinput.FileInput(os.path.join(root,filename), inplace=True, backup='') as file:
+                for line in file:
+                    print(line.replace(velho, novo), end='')
+            file.close()
+
+
+###PRINCIPAL####
+
+de,para = retorna_depara("de_para.csv")
+Dir = input("Diretorio:")
+os.chdir(Dir)
+for i in range(len(de)):
+    print("Atualizando de "+de[i] +" para "+para[i])
+    substituir(Dir, de[i], para[i])
+
+#velho = input("Texto velho:")
+#novo = input("Texto novo:")
+
+#for (dirpath, dirnames, filenames) in walk(Dir):
+#    print (dirnames)
+#    print (filenames)
+
+#Filelist = os.listdir()
+#Eprint('File list: ',Filelist)
